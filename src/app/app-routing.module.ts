ed90404689fd8fc4342827e1f49b1af80c45e476
= <=>
2023-10-10 15:19:40 +0200

    Première page commencer

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core"
+import { RouterModule, Routes } from "@angular/router"
+import {ConnexionComponent} from "./connexion/connexion.component";
+
+
+const routes: Routes = [
+  { path: "", component: ConnexionComponent },
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {
+}
