11fab18dc72e165112694d102572d3fd4e637d47
= <=>
2023-10-16 09:41:32 +0200

    Construction page question

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
 import {ConnexionComponent} from "./connexion/connexion.component";
+import {QuestionsComponent} from "./questions/questions.component";
 
 
 const routes: Routes = [
   { path: "", component: ConnexionComponent },
+  { path: "question", component: QuestionsComponent },
 ]
 
 @NgModule({
