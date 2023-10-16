d917d72885d42719e37c096cf0777dcd0930d137
HugoEPF <125960159+HugoEPF@users.noreply.github.com>
2023-10-16 14:32:13 +0200

    Page de questions (#5)
    * Construction page question
    
    * Page questions finit
    
    ---------
    
    Co-authored-by: = <=>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
-import { HeaderComponent } from './header/header.component';
+import { QuestionsComponent } from './questions/questions.component';
 
 @NgModule({
-  declarations: [
+  declarations:[
     AppComponent,
-    HeaderComponent
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
