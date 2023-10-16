b287d337e888ebc8a87e50f6e88a4c113565999c
ThibaultClt <123541622+ThibaultClt@users.noreply.github.com>
2023-10-16 16:28:07 +0200

    Création pages choix themes + lien + classement (#6)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
 import {ConnexionComponent} from "./connexion/connexion.component";
+import {ChoixThemeComponent} from "./choix-theme/choix-theme.component";
+import {LienComponent} from "./lien/lien.component";
+import {ClassementComponent} from "./classement/classement.component";
 import {QuestionsComponent} from "./questions/questions.component";
+
 
 
 const routes: Routes = [
   { path: "", component: ConnexionComponent },
-  { path: "question", component: QuestionsComponent },
+  { path: "choix_theme", component: ChoixThemeComponent},
+  { path: "lien", component: LienComponent},
+  { path: "classement", component: ClassementComponent},
+  { path: "question", component: QuestionsComponent }
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
+import { ChoixThemeComponent } from './choix-theme/choix-theme.component';
+import { LienComponent } from './lien/lien.component';
+import { ClassementComponent } from './classement/classement.component';
 import { QuestionsComponent } from './questions/questions.component';
+
 
 @NgModule({
   declarations:[
     AppComponent,
+    ChoixThemeComponent,
+    LienComponent,
+    ClassementComponent,
     QuestionsComponent
   ],
   imports: [
