54aad7bc0d9baca29c1fd7513ea21bf0bd9c759a
thibault <thibault.collet@epfedu.fr>
2023-11-20 15:12:19 +0100

    Ajout pages admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,49 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
-import { ChoixThemeComponent } from './choix-theme/choix-theme.component';
-import { LienComponent } from './lien/lien.component';
-import { ClassementComponent } from './classement/classement.component';
-import { QuestionsComponent } from './questions/questions.component';
+import {ChoixThemeComponent} from './choix-theme/choix-theme.component';
+import {LienComponent} from './lien/lien.component';
+import {ClassementComponent} from './classement/classement.component';
+import {QuestionsComponent} from './questions/questions.component';
 import {HttpClientModule} from "@angular/common/http";
-import { PageAdminComponent } from './page-admin/page-admin.component';
-import { AccueilAdminComponent } from './accueil-admin/accueil-admin.component';
-import { ChoixThemeAdminComponent } from './choix-theme-admin/choix-theme-admin.component';
-import { CreationQuizComponent } from './creation-quiz/creation-quiz.component';
-import { GestionQuizComponent } from './gestion-quiz/gestion-quiz.component';
-import { GestionUserComponent } from './gestion-user/gestion-user.component';
+import {AccueilAdminComponent} from './accueil-admin/accueil-admin.component';
+import {ChoixThemeAdminComponent} from './choix-theme-admin/choix-theme-admin.component';
+import {CreationQuizComponent} from './creation-quiz/creation-quiz.component';
+import {GestionQuizComponent} from './gestion-quiz/gestion-quiz.component';
+import {GestionUserComponent} from './gestion-user/gestion-user.component';
+import {FormsModule} from "@angular/forms";
+import {ConnexionComponent} from "./connexion/connexion.component";
+import { NavbarAdminComponent } from './navbar-admin/navbar-admin.component';
+import { EditQuizComponent } from './edit-quiz/edit-quiz.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
 
 
 @NgModule({
-  declarations:[
+  declarations: [
     AppComponent,
     ChoixThemeComponent,
+    ConnexionComponent,
     LienComponent,
     ClassementComponent,
     QuestionsComponent,
-    PageAdminComponent,
     AccueilAdminComponent,
     ChoixThemeAdminComponent,
     CreationQuizComponent,
     GestionQuizComponent,
-    GestionUserComponent
+    GestionUserComponent,
+    NavbarAdminComponent,
+    EditQuizComponent,
+    EditUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
