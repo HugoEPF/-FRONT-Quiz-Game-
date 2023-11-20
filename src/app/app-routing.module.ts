31f9d755f75e3776f7a0bc010e72af3837edf221
ThibaultClt <123541622+ThibaultClt@users.noreply.github.com>
2023-11-20 15:14:11 +0100

    Création pages admin (#7)
    * Ajout pages admin
    
    * Ajout pages admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from "@angular/core"
-import { RouterModule, Routes } from "@angular/router"
+import {NgModule} from "@angular/core"
+import {RouterModule, Routes} from "@angular/router"
 import {ConnexionComponent} from "./connexion/connexion.component";
 import {ChoixThemeComponent} from "./choix-theme/choix-theme.component";
 import {LienComponent} from "./lien/lien.component";
 import {ClassementComponent} from "./classement/classement.component";
 import {QuestionsComponent} from "./questions/questions.component";
+import {AccueilAdminComponent} from "./accueil-admin/accueil-admin.component";
+import {GestionQuizComponent} from "./gestion-quiz/gestion-quiz.component";
+import {GestionUserComponent} from "./gestion-user/gestion-user.component";
+import {ChoixThemeAdminComponent} from "./choix-theme-admin/choix-theme-admin.component";
+import {CreationQuizComponent} from "./creation-quiz/creation-quiz.component";
+import {EditQuizComponent} from "./edit-quiz/edit-quiz.component";
+import {EditUserComponent} from "./edit-user/edit-user.component";
 
 
 const routes: Routes = [
-  { path: "", component: ConnexionComponent },
-  { path: "choix_theme", component: ChoixThemeComponent},
-  { path: "lien/:genre", component: LienComponent},
-  { path: "classement", component: ClassementComponent},
-  { path: "question/:genre/:id", component: QuestionsComponent }
+  {path: "", component: ConnexionComponent},
+  {path: "choix_theme", component: ChoixThemeComponent},
+  {path: "lien/:genre", component: LienComponent},
+  {path: "classement", component: ClassementComponent},
+  {path: "question/:genre/:id", component: QuestionsComponent},
+  {path: "question", component: QuestionsComponent},
+  {path: "accueil_admin", component: AccueilAdminComponent},
+  {path: "gestion_quiz", component: GestionQuizComponent},
+  {path: "gestion_user", component: GestionUserComponent},
+  {path: "choix_theme_admin", component: ChoixThemeAdminComponent},
+  {path: "creation_quiz", component: CreationQuizComponent},
+  {path: "edit_quiz", component: EditQuizComponent},
+  {path: "edit_user", component: EditUserComponent},
 
 ]
 
---
+++ src/app/gestion-quiz/gestion-quiz.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-quiz',
+  templateUrl: './gestion-quiz.component.html',
+  styleUrls: ['./gestion-quiz.component.css']
+})
+export class GestionQuizComponent {
+
+}
