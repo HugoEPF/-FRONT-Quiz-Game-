df45c27e240d115077cdd3377fce83e734f06f22
HugoEPF <125960159+HugoEPF@users.noreply.github.com>
2023-11-25 23:52:55 +0100

    Enchainement question (#12)
    * Enchainement question fonctionnelle, renvoie vers page d'accueil
    
    * package fix
    
    * correction import inutilisés
    
    ---------
    
    Co-authored-by: = <=>

--- src/app/services/quizz.service.ts
+++ src/app/services/quizz.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import {Observable, of} from "rxjs"
 import { HttpClient } from "@angular/common/http"
 import {Quizz} from "../models/Quizz";
-import {Users} from "../models/Users";
 
 @Injectable({
   providedIn: 'root'
