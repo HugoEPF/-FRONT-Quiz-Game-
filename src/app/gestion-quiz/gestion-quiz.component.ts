b3d6b8b26dfc06bb8557399bd87409820f78184b
thibault <thibault.collet@epfedu.fr>
2023-11-26 20:22:42 +0100

    Suppression quiz

--- src/app/gestion-quiz/gestion-quiz.component.ts
+++ src/app/gestion-quiz/gestion-quiz.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import {Observable} from "rxjs";
-import {Quizz} from "../models/Quizz";
-import {QuizzService} from "../services/quizz.service";
+import {Component} from '@angular/core';
+import {map, Observable} from 'rxjs';
+import {Quizz} from '../models/Quizz';
+import {QuizzService} from '../services/quizz.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-gestion-quiz',
@@ -10,11 +11,25 @@ import {QuizzService} from "../services/quizz.service";
 })
 export class GestionQuizComponent {
 
+  quizz$: Observable<Quizz[]> = this.quizzService.findAll();
 
-  quizz$: Observable<Quizz[]> = this.quizzService.findAll()
+  constructor(
+    private quizzService: QuizzService,
+    private _route: ActivatedRoute,
+    private router: Router,
+  ) {
+  }
 
-  constructor(private quizzService: QuizzService) {
-
+  deleteQuestion(quizz: Quizz): void {
+    if (quizz.id !== undefined) {
+      const QuizzId: bigint = BigInt(quizz.id);
+      this.quizzService.delete(QuizzId).subscribe(() => {
+        // Filtrer les quizzes pour exclure celui qui vient d'être supprimé
+        this.quizz$ = this.quizz$.pipe(
+          map(quizzes => quizzes.filter(q => q.id !== quizz.id))
+        );
+      });
+    }
   }
 
 }
