4694faf14035e52ce856b45e9e346eca4416b114
= <=>
2023-11-26 17:15:45 +0100

    Ajout score, suppression bouton rejouer

--- src/app/classement/classement.component.ts
+++ src/app/classement/classement.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Users} from "../models/Users";
+import {QuizzService} from "../services/quizz.service";
 
 @Component({
   selector: 'app-classement',
@@ -8,12 +9,16 @@ import {Users} from "../models/Users";
 })
 export class ClassementComponent implements OnInit {
   user: Users | null = null;
-
+  score: number | undefined
+  constructor(quizzServide:QuizzService) {
+  }
   ngOnInit(): void {
     const userString = localStorage.getItem('user');
     if (userString) {
       this.user = JSON.parse(userString);
       console.log(this.user);
     }
+    this.score = Number(localStorage.getItem('score'))
+
   }
 }
