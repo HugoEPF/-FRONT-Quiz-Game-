dd61c49c2768d23495ac3b7f067462d3f4a46572
HugoEPF <125960159+HugoEPF@users.noreply.github.com>
2023-11-26 17:19:23 +0100

    Modif classement (#18)
    * Ajout page des scores
    
    * Ajout score, suppression bouton rejouer
    
    ---------
    
    Co-authored-by: thibault <thibault.collet@epfedu.fr>
    Co-authored-by: = <=>

--- src/app/classement/classement.component.ts
+++ src/app/classement/classement.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Users} from "../models/Users";
+import {QuizzService} from "../services/quizz.service";
 
 @Component({
   selector: 'app-classement',
   templateUrl: './classement.component.html',
   styleUrls: ['./classement.component.css']
 })
-export class ClassementComponent {
+export class ClassementComponent implements OnInit {
+  user: Users | null = null;
+  score: number | undefined
+  constructor(quizzServide:QuizzService) {
+  }
+  ngOnInit(): void {
+    const userString = localStorage.getItem('user');
+    if (userString) {
+      this.user = JSON.parse(userString);
+      console.log(this.user);
+    }
+    this.score = Number(localStorage.getItem('score'))
 
+  }
 }
